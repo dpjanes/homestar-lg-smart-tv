e635ddfabc2c634996215719e2d0751a763a5a6e
David Janes <davidjanes@davidjanes.com>
2016-08-27 09:36:16 -0400

    add back hdmi.1

--- models/LGSmartTV.js
+++ models/LGSmartTV.js
@@ -9,23 +9,4 @@
 exports.binding = {
     bridge: require('../LGSmartTVBridge').Bridge,
     model: require('./lg-smart-tv.json'),
-    mapping: {
-        band: {
-            "iot-purpose:band.service.anyplace-tv": "anyplace.tv",
-            "iot-purpose:band.service.cinemanow": "cinemanow",
-            "iot-purpose:band.browser": "com.webos.app.browser",
-            "iot-purpose:band.camera": "com.webos.app.camera",
-            "iot-purpose:band.hdmi": "com.webos.app.hdmi1",
-            "iot-purpose:band.hdmi#1": "com.webos.app.hdmi1",
-            "iot-purpose:band.hdmi#2": "com.webos.app.hdmi2",
-            "iot-purpose:band.tv": "com.webos.app.livetv",
-            "iot-purpose:band.service.miracast": "com.webos.app.miracast",
-            "iot-purpose:band.info.notifications": "com.webos.app.notificationcenter",
-            "iot-purpose:band.service.smartshare": "com.webos.app.smartshare",
-            "iot-purpose:band.info.guide": "com.webos.app.tvuserguide",
-            "iot-purpose:band.service.crackle": "crackle",
-            "iot-purpose:band.service.netflix": "netflix",
-            "iot-purpose:band.service.youtube": "youtube.leanback.v4",
-        }
-    }
 };
